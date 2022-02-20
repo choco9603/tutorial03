ea5beaa67506fbcc40744773823af4f263b22c2a
choco9603 <309choco9603@gmail.com>
2022-02-20 21:55:49 +0900

    moon

---
+++ main3.js
@@ -0,0 +1,104 @@
+window.addEventListener('DOMContentLoaded',init);
+
+function init(){
+  //サイズ指定
+  const width = 960;
+  const height = 540;
+  //角度
+  let rot = 0;
+  //マウス座標
+  let mouseX = 0;
+  
+  //レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#myCanvas3'),
+  });
+  renderer.setSize(width,height);
+  
+  //シーンを作成
+  const scene = new THREE.Scene();
+  
+  //カメラを作成
+  const camera = new THREE.PerspectiveCamera(45,width/height);
+  //camera.position.set(0,0,+1000);
+  
+  //球体を作成
+  const geometry = new THREE.SphereGeometry(300,30,30);
+  
+  //マテリアルを作成(色指定)
+  //const material = new THREE.MeshStandardMaterial({color:0xff0000});
+  
+  //画像を読み込む
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load('/moon_map.tiff');
+  //マテリアルテクスチャーを設定
+  const material = new THREE.MeshStandardMaterial({
+    map:texture,
+    side:THREE.DoubleSide,
+  });
+  //メッシュ作成
+  const moonMesh = new THREE.Mesh(geometry,material);
+  
+  //3D空間にメッシュを追加
+  scene.add(moonMesh);
+  
+  //星屑を作成
+  createStarField();
+  
+  function createStarField(){
+    //頂点情報を作成
+    const vertices = [];
+    for(let i = 0; i < 1000; i++){
+      const x = 3000 * (Math.random()-0.5);
+      const y = 3000 * (Math.random()-0.5);
+      const z = 3000 * (Math.random()-0.5);
+      
+      vertices.push(x,y,z);
+    }
+    
+    //形状データを作成
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position',new THREE.Float32BufferAttribute(vertices,3));
+    //マテリアルを作成
+    const material = new THREE.PointsMaterial({
+      size:10,
+      color:0xffffff,
+    });
+    //星屑追加
+    const mesh = new THREE.Points(geometry,material);
+    scene.add(mesh);
+  }
+  
+  //平行光源
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1,1,2);
+  //シーンに追加
+  scene.add(directionalLight);
+  
+  document.addEventListener("mousemove",(event)=>{
+    mouseX = event.pageX;
+  });
+  
+  tick();
+  
+  //毎フレーム時に実行されるループイベント
+  function tick(){
+    //マウスの位置に応じて角度を設定
+    //マウスのX座標がステージの幅の何％の位置にあるか調べて、360度で乗算
+    const targetRotX = (mouseX/window.innerWidth)*360;
+    //イージングの公式を用いて滑らかにする(値+=(目標値-現在の値)*減速値)
+    rot += (targetRotX - rot)*0.02;
+    //毎フレーム角度を0.5度足す
+    //rot += 0.5;
+    //ラジアンに変換
+    const radian = (rot * Math.PI)/100;
+    camera.position.x = 1000 * Math.sin(radian);
+    camera.position.z = 1000 * Math.cos(radian);
+    
+    //カメラ方向を原点へ固定
+    camera.lookAt(new THREE.Vector3(0,0,0));
+    //レンダリング
+    renderer.render(scene,camera);
+    requestAnimationFrame(tick);
+  }
+}
---
+++ main5.js
@@ -0,0 +1,110 @@
+window.addEventListener('DOMContentLoaded',init);
+
+function init(){
+  //サイズ指定
+  const width = 960;
+  const height = 540;
+  //角度
+  let rot = 0;
+  //マウス座標
+  let mouseX = 0;
+  
+  //レンダラーを作成
+  const canvasElement = document.querySelector('#myCanvas5');
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvasElement,
+  });
+  renderer.setSize(width,height);
+  
+  //シーンを作成
+  const scene = new THREE.Scene();
+  
+  //カメラを作成
+  const camera = new THREE.PerspectiveCamera(45,width/height);
+  camera.position.set(0,0,1000);
+  
+  //カメラコントローラーを作成
+  const controls = new THREE.OrbitControls(camera,canvasElement);
+  //滑らかにカメラコントローラーを制御
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
+  
+  //球体を作成
+  const geometry = new THREE.SphereGeometry(300,30,30);
+  
+  //マテリアルを作成(色指定)
+  //const material = new THREE.MeshStandardMaterial({color:0xff0000});
+  
+  //画像を読み込む
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load('/moon_map.tiff');
+  //マテリアルテクスチャーを設定
+  const material = new THREE.MeshStandardMaterial({
+    map:texture,
+    side:THREE.DoubleSide,
+  });
+  //メッシュ作成
+  const moonMesh = new THREE.Mesh(geometry,material);
+  
+  //3D空間にメッシュを追加
+  scene.add(moonMesh);
+  
+  //星屑を作成
+  createStarField();
+  
+  function createStarField(){
+    //頂点情報を作成
+    const vertices = [];
+    for(let i = 0; i < 1000; i++){
+      const x = 3000 * (Math.random()-0.5);
+      const y = 3000 * (Math.random()-0.5);
+      const z = 3000 * (Math.random()-0.5);
+      
+      vertices.push(x,y,z);
+    }
+    
+    //形状データを作成
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position',new THREE.Float32BufferAttribute(vertices,3));
+    //マテリアルを作成
+    const material = new THREE.PointsMaterial({
+      size:10,
+      color:0xffffff,
+    });
+    //星屑追加
+    const mesh = new THREE.Points(geometry,material);
+    scene.add(mesh);
+  }
+  
+  //平行光源
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1,1,2);
+  //シーンに追加
+  scene.add(directionalLight);
+  
+  document.addEventListener("mousemove",(event)=>{
+    mouseX = event.pageX;
+  });
+  
+  tick();
+  
+  //毎フレーム時に実行されるループイベント
+  function tick(){
+    //回転
+    //moonMesh.rotation.y -= 0.01;
+    //ラジアンに変換
+    //const radian = (rot * Math.PI)/100;
+    //camera.position.x = 1000 * Math.sin(radian);
+    //camera.position.z = 1000 * Math.cos(radian);
+    
+    //カメラ方向を原点へ固定
+    //camera.lookAt(new THREE.Vector3(0,0,0));
+    
+    //カメラコントローラーを更新
+    controls.update();
+    
+    //レンダリング
+    renderer.render(scene,camera);
+    requestAnimationFrame(tick);
+  }
+}
