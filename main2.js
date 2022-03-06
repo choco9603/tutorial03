21f15c9dbd94b9f1fb45c7152079d1f3e7c8c037
choco9603 <98647882+choco9603@users.noreply.github.com>
2022-03-06 19:32:36 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -25,17 +25,17 @@ function init(){
   //球体を作成
   const geometry = new THREE.SphereGeometry(300,30,30);
   
-  //マテリアルを作成(色指定)
-  //const material = new THREE.MeshStandardMaterial({color:0xff0000});
+  const material = new THREE.MeshPhongMaterial({color:0x0000ff,wireframe:true});
+  
   
   //画像を読み込む
-  const loader = new THREE.TextureLoader();
-  const texture = loader.load('https://github.com/choco9603/tutorial03/blob/master/imgs/moon_map.jpeg?raw=true');
-  //マテリアルテクスチャーを設定
-  const material = new THREE.MeshStandardMaterial({
-    map:texture,
-    side:THREE.DoubleSide,
-  });
+  // const loader = new THREE.TextureLoader();
+  // const texture = loader.load('/moon_map.jpeg');
+  // //マテリアルテクスチャーを設定
+  // const material = new THREE.MeshStandardMaterial({
+  //   map:texture,
+  //   side:THREE.DoubleSide,
+  // });
   //メッシュ作成
   const moonMesh = new THREE.Mesh(geometry,material);
   
